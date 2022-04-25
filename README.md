57f72c4d0b4fe2e314f2e3867f50158a443cece9
Peter Szekeli <97154653+peterszekelip1@users.noreply.github.com>
2022-04-25 15:48:47 +0200

    demo-002: sonarqube quality gate check (#2)

--- README.md
+++ README.md
@@ -2,26 +2,30 @@
 
 ## General notes
 
-- deafult branch is master (rename to "main" branch beforehand, if needed)
+- default branch is master (rename to "main" branch beforehand, if needed)
 
-## etbaas-backend
+## dotnet csharp
 
 - Requirment: `dotnet tool install --global dotnet-sonarscanner`
 - Generate token
 - execute:
 
-> dotnet sonarscanner begin /k:"{{ SONAR_PROJECT_NAME}}" /d:sonar.host.url="{{ SONAR_HOST_URL }}"  /d:sonar.login="{{ SONAR_TOKEN }}"
->
-> dotnet build ...
->dotnet test ...
->
-> dotnet sonarscanner end /d:sonar.login="{{ SONAR_TOKEN }}"
+```cs
 
-## etbaas-frontend
+ dotnet sonarscanner begin /k:"{{ SONAR_PROJECT_KEY }}" /d:sonar.host.url="{{ SONAR_HOST_URL }}"  /d:sonar.login="{{ SONAR_TOKEN }}"
+
+ dotnet build ...
+ dotnet test ...
+
+ dotnet sonarscanner end /d:sonar.login="{{ SONAR_TOKEN }}"
+
+```
+
+## javascript / typescript
 
 - Requirment: download and install sonar-scanner (OS dependent)
 - Generate token
 - execute:  
-  `sonar-scanner.bat -D"sonar.projectKey=etbaas-frontend" -D"sonar.sources=." -D"sonar.host.url={{ SONAR_HOST_URL }}" -D"sonar.login={{ SONAR_TOKEN }}"`
+  `sonar-scanner.bat -D"sonar.projectKey={{ SONAR_PROJECT_KEY }}" -D"sonar.sources=." -D"sonar.host.url={{ SONAR_HOST_URL }}" -D"sonar.login={{ SONAR_TOKEN }}"`
 
 ## sonarqube-demo
--- src/ClassLibrary1.Tests/CalculatorTests.cs
+++ src/ClassLibrary1.Tests/CalculatorTests.cs
@@ -6,14 +6,91 @@ namespace ClassLibrary1.Tests;
 public class UnitTest1
 {
     [Theory]
-    [InlineData(5)]
-    [InlineData(123)]
-    [InlineData(-3)]
-    public void Add_Zero_ReturnsOriginalValue(int a)
+    [InlineData(5, 3, 8)]
+    [InlineData(123, -100, 23)]
+    [InlineData(-3, 0, -3)]
+    public void Add(int a, int b, int expectedResult)
     {
         var calculator = new Calculator();
-        var result = calculator.Add(a, 0);
+        var result = calculator.Add(a, b);
 
-        result.Should().Be(a);
+        result.Should().Be(expectedResult);
+    }
+
+    [Theory]
+    [InlineData(5, 3, 2)]
+    [InlineData(123, -100, 223)]
+    [InlineData(-3, 0, -3)]
+    public void Subtract(int a, int b, int expectedResult)
+    {
+        var calculator = new Calculator();
+        var result = calculator.Subtract(a, b);
+
+        result.Should().Be(expectedResult);
+    }
+
+    [Theory]
+    [InlineData(5, 3, 15)]
+    [InlineData(123, -1, -123)]
+    [InlineData(-3, 0, 0)]
+    [InlineData(42, 1, 42)]
+    public void Multiply(int a, int b, int expectedResult)
+    {
+        var calculator = new Calculator();
+        var result = calculator.Multiply(a, b);
+
+        result.Should().Be(expectedResult);
+    }
+
+    [Theory]
+    [InlineData(6, 3, 2)]
+    [InlineData(123, -1, -123)]
+    [InlineData(42, 1, 42)]
+    public void Divide(int a, int b, int expectedResult)
+    {
+        var calculator = new Calculator();
+        var result = calculator.Divide(a, b);
+
+        result.Should().Be(expectedResult);
+    }
+
+    [Theory]
+    [InlineData(1, 0, 1)]
+    [InlineData(1, 99, 1)]
+    [InlineData(2, 10, 1024)]
+    public void Power(int a, int b, int expectedResult)
+    {
+        var calculator = new Calculator();
+        var result = calculator.Power(a, b);
+
+        result.Should().Be(expectedResult);
+    }
+
+    [Theory]
+    [InlineData(0, 1)]
+    [InlineData(1, 1)]
+    [InlineData(2, 2)]
+    [InlineData(3, 6)]
+    [InlineData(10, 3628800)]
+    public void Factorial(int a, int expectedResult)
+    {
+        var calculator = new Calculator();
+        var result = calculator.Factorial(a);
+
+        result.Should().Be(expectedResult);
+    }
+
+    [Theory]
+    [InlineData(0, 0)]
+    [InlineData(1, 1)]
+    [InlineData(2, 2)]
+    [InlineData(3, 3)]
+    [InlineData(10, 89)]
+    public void Fibonacci(int a, int expectedResult)
+    {
+        var calculator = new Calculator();
+        var result = calculator.Fibonacci(a);
+
+        result.Should().Be(expectedResult);
     }
 }
